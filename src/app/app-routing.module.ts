f0926cc9906e65d7063dab7c7b878d8c67ad48a1
Supun Chalaka <supunchalaka@gmail.com>
2022-03-22 23:06:14 +0530

    test commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [];
+//test
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
